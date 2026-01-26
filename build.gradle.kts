a9bfc2beed9ded145d1daa8ac3eccbd26b6ade3c
Axel Howind <axh@dua3.com>
2026-01-26 03:04:02 +0100

    Remove '-Xlint:unchecked' from compiler arguments

--- build.gradle.kts
+++ build.gradle.kts
@@ -226,12 +226,12 @@ allprojects {
     if (!project.name.endsWith("-bom")) {
         tasks.compileJava {
             options.encoding = "UTF-8"
-            options.compilerArgs.addAll(listOf("-Xlint:deprecation", "-Xlint:-module", "-Xlint:unchecked"))
+            options.compilerArgs.addAll(listOf("-Xlint:deprecation", "-Xlint:-module"))
             options.javaModuleVersion.set(provider { project.version as String })
         }
         tasks.compileTestJava {
             options.encoding = "UTF-8"
-            options.compilerArgs.addAll(listOf("-Xlint:deprecation", "-Xlint:-module", "-Xlint:unchecked"))
+            options.compilerArgs.addAll(listOf("-Xlint:deprecation", "-Xlint:-module"))
         }
         tasks.javadoc {
             (options as StandardJavadocDocletOptions).apply {
