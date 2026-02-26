2ff179e963a4c2bbb2ffd4da692e6ff36981c03b
Axel Howind <axh@dua3.com>
2026-02-26 03:20:28 +0100

    Update project version to `4.1.0-beta` and upgrade dependencies: `dua3-utility` to `22.0.0-beta`, `SLB4J` to `0.7.0-beta`

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@
 import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 rootProject.name = "fx-application"
-val projectVersion = "4.0.1"
+val projectVersion = "4.1.0-beta"
 
 include(":fx-application-fxml")
 
